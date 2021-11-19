76d7aced8509a301a7d351e2794378d7b59c0cf4
Joachim Baran <joachim.baran@gmail.com>
2021-11-19 18:05:35 -0500

    Added second job for linting and commit/push.

--- Package.swift
+++ Package.swift
@@ -6,7 +6,7 @@ let package = Package(
     products: [
         .library(
             name: "Example",
-            targets: ["Example"]),
+            targets: ["Example"])
     ],
     dependencies: [
     ],
@@ -18,6 +18,6 @@ let package = Package(
             ]),
         .testTarget(
             name: "ExampleTests",
-            dependencies: ["Example"]),
+            dependencies: ["Example"])
     ]
 )
--- Sources/Example/Example.swift
+++ Sources/Example/Example.swift
@@ -1,3 +1,7 @@
-func sendGreetings() -> String {
+public func sendGreetings() -> String {
     return "Hello World!"
+}
+
+public func linterFixExample(x :Int) -> Int {
+    return x
 }
