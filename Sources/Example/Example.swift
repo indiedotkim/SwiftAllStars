82f82bcb28683bcde4fe83c25e4b75d988f4ac5a
Joachim Baran <joachim.baran@gmail.com>
2021-11-19 20:15:49 -0500

    Changed example, so that it does not always auto-correct.

--- Sources/Example/Example.swift
+++ Sources/Example/Example.swift
@@ -2,6 +2,8 @@ public func sendGreetings() -> String {
     return "Hello World!"
 }
 
-public func linterFixExample(whoopsie :Int) -> Int {
+// To test the linter's auto-correct, change the colon in the parameter
+// as follows: from `whoopse: Int` to `whoopsie :Int`
+public func linterFixExample(whoopsie: Int) -> Int {
     return whoopsie
 }
